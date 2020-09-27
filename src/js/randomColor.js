f3788b623f3cc1cc8bd1a83937ebdbd304ba57b4
Yevhenii Korkots <atc0884@gmail.com>
2020-09-27 22:43:11 +0300

    branch changed

--- src/js/randomColor.js
+++ src/js/randomColor.js
@@ -1,5 +1,50 @@
 'use strict';
+// import colors from './colors';
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-action="start"]'),
+  stopBtn: document.querySelector('button[data-action="stop"]'),
+};
 
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+
+//Какой метод добавления стиля лучше???
+const colorChange = color => {
+  refs.body.setAttribute('style', `background-color:${color}`);
+  // refs.body.style.cssText = 'background-color: `${color}`';
+  // refs.body.style.backgroundColor = `${color}`;
+};
+
+const colorSwitch = {
+  intervalId: null,
+  isActive: false,
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    this.isActive = true;
+    this.intervalId = setInterval(() => {
+      colorChange(colors[randomIntegerFromInterval(0, colors.length - 1)]);
+    }, 1000);
+  },
+
+  stop() {
+    clearInterval(this.intervalId);
+    this.intervalId = null;
+    this.isActive = false;
+  },
+};
+
+refs.startBtn.addEventListener('click', colorSwitch.start.bind(colorSwitch));
+refs.stopBtn.addEventListener('click', colorSwitch.stop.bind(colorSwitch));
